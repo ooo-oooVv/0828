d3b3b38b90e6ce53008b20df05cf0062ef0986b0
ooo-oooVv <yuyeeun@naver.com>
2022-08-28 11:50:36 +0900

    배너프로젝트제작하기

--- js/banner.js
+++ js/banner.js
@@ -1,13 +1,13 @@
 // $.noConflict();
 // var bby$ = jQuery;
 
-$(function(){
+$(function(){ // = 화면 실행 직후 터짐 (=doucument.ready)
     var _classNm = 'next'
     var autoTimer = setInterval(function(){
         _setTimefun($('.timer').html(), _classNm)
         }, 1000)
 
-        $('button').click(function(){
+        $('button').click(function(){ //다음 버튼 클릭 시 실행
                 // 다음 버튼 실행
                 clearInterval(autoTimer); // autoTimer 기능 없애줘!
                 _classNm = $(this).attr('class'); //class 이름 저장
@@ -21,6 +21,9 @@ $(function(){
         })
 
     function _setTimefun(count, dir){
+        // 함수의 용도 -> 두가지 다른 배반적 조건에 숫자출력
+        // 앞뒤버튼이 있어서 증가, 혹은 가감 숫자 출력.
+        // 첫번째 인자는 숫자자리, 두번째 인자는 next 값이 true or false
         if (dir == 'next') {
             if(count < 4) count++; else count = 0;
         }
